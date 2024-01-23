0ab73974abc888c9af194a4898bd962183969b78
Shaunsinthewind777 <131711280+Shaunsinthewind777@users.noreply.github.com>
2024-01-23 15:37:18 -0800

    Update arithmetic_arranger.py

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -1,9 +1,4 @@
 
-
-
-
-
-
 def arithmetic_arranger(problems, show_answers=False):
     arranged_problems = {"top": [], "bottom": [], "line": [], "result": []}
 
@@ -28,5 +23,6 @@ def arithmetic_arranger(problems, show_answers=False):
 problems = ["32 + 698", "3801 - 2", "45 + 43", "123 + 49"]
 result_with_answers = arithmetic_arranger(problems, show_answers=True)
 print(result_with_answers)
+
 
 
